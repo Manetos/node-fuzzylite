5003a5dc237800a2b1dca9c6065ec092da76dd47
Erik Klockare <erik.klockare@scypho.com>
2015-11-18 17:36:50 +0100

    added controller interface test

---
+++ test/controller-interface-spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var proxyquire = require('proxyquire');
+var sinon = require('sinon');
+var assert = require('assert');
+
+describe('controller-interface', function() {
+    beforeEach(function() {
+        this.process = {
+            exec: sinon.stub().returns({
+                on: sinon.spy()
+            })
+        };
+
+        this.ControllerInterface = proxyquire.noCallThru()
+        ('../lib/controller-interface', {
+            'child_process': this.process,
+        });
+
+        this.controllerInterface = new this.ControllerInterface();
+    });
+
+    it('should give error on string input', function(done) {
+        var self = this;
+        this.process.exec.yields(null, '_');
+        this.controllerInterface.runController(['string', 'input'],
+            function(err) {
+                assert(err);
+                assert(!self.process.exec.called);
+                done();
+        });
+    });
+
+    it('should be called ones with valid input', function(done) {
+        var self = this;
+        this.process.exec.yields(null, '_');
+        this.controllerInterface.runController([0.1, 0.2],
+            function() {
+                assert(self.process.exec.calledOnce);
+                done();
+        });
+    });
+});
