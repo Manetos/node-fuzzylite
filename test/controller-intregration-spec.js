ee5fb6895bcd1dbf265536c3517ff5aa4a579e48
Erik Klockare <erik.klockare@scypho.com>
2015-11-24 15:04:00 +0100

    changed test controller to have two inputs

--- test/controller-intregration-spec.js
+++ test/controller-intregration-spec.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//var proxyquire = require('proxyquire');
-//var sinon = require('sinon');
 var assert = require('assert');
 var ControllerInterface = require('../lib/controller-interface');
 
@@ -11,19 +9,17 @@ describe('controller-interface', function() {
     });
 
     it('it should give an expected output on input', function(done) {
-        //var self = this;
-        this.controllerInterface.runController([0.2],
+        this.controllerInterface.runController([-0.4, 3.1],
             function(err, result) {
-                assertFloatEqual(result, 1.5);
+                assertFloatEqual(result, 0.5);
                 done();
         });
     });
 
     it('it should give an expected output on input', function(done) {
-        //var self = this;
-        this.controllerInterface.runController([0.8],
+        this.controllerInterface.runController([0.2, 1.1],
             function(err, result) {
-                assertFloatEqual(result, 0.5);
+                assertFloatEqual(result, 1.5);
                 done();
         });
     });
