28a151bc3b2531608f99c3308cde9c5809b311c6
Linus Wallgren <linus.wallgren@scypho.com>
2016-06-28 17:13:44 +0200

    Update build-system to work cross-platform

--- build-fuzzylite.sh
+++ build-fuzzylite.sh
@@ -6,7 +6,8 @@ set -u
 mkdir -p fuzzylite/fuzzylite/release
 cd fuzzylite/fuzzylite/release
 
-# We use g++-4.8 as >4.8 requires >= GLIBCXX 3.4.20, but only 3.4.19 is
-# available on ubuntu 14.04
-cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=$(which g++-4.8) -DCMAKE_BUILD_TYPE=Release -DFL_BACKTRACE=ON -DFL_USE_FLOAT=OFF -DFL_CPP11=OFF
+# We require GLIBCXX >= 3.4.20, which is something you get with g++-4.8 and
+# newer. Keep in mind that for example ubuntu 14.04 has a version of g++ that
+# is too old.
+cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DFL_BACKTRACE=ON -DFL_USE_FLOAT=OFF -DFL_CPP11=OFF
 make
