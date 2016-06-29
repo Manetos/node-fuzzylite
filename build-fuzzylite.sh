3248211c9f9ea96ccce0a23a268c458d1687bf31
Linus Wallgren <linus.wallgren@scypho.com>
2016-06-29 11:36:37 +0200

    If c++ compiler is undefined, use g++4.8
    This is to deal with both someone overriding the compiler and otherwise
    to use one that will work on ubuntu 14.04

--- build-fuzzylite.sh
+++ build-fuzzylite.sh
@@ -6,8 +6,8 @@ set -u
 mkdir -p fuzzylite/fuzzylite/release
 cd fuzzylite/fuzzylite/release
 
-# We require GLIBCXX >= 3.4.20, which is something you get with g++-4.8 and
-# newer. Keep in mind that for example ubuntu 14.04 has a version of g++ that
-# is too old.
+# Usinga newer version than g++-4.8 will make this not run on ubuntu 14.04, as
+# that only has GLIBCXX 3.4.19 available, and g++4.8 requires GLIBCXX >= 3.4.20
+export CXX=${CXX:-$(which g++-4.8)}
 cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DFL_BACKTRACE=ON -DFL_USE_FLOAT=OFF -DFL_CPP11=OFF
 make
